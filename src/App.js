ff8d0f40cdd7c0320598261ec90f5766db7d5f17
OmarRaihan <omarraihan91@gmail.com>
2022-04-04 15:24:43 +0600

    heading and img added

--- src/App.js
+++ src/App.js
@@ -12,14 +12,14 @@ function App() {
   return (
     <div className="App">
       <Header></Header>
-      {/* <Routes>
+      <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/home" element={<Home></Home>}></Route>
         <Route path="/reviews" element={<Reviews></Reviews>}></Route>
         <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
         <Route path="/blogs" element={<Blogs></Blogs>}></Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
-      </Routes> */}
+      </Routes>
     </div>
   );
 }

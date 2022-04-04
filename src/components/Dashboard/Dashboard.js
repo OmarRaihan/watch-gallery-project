83d32ac46cbc2be3e5e418214f9bc93ebc739f0a
OmarRaihan <omarraihan91@gmail.com>
2022-04-04 22:50:42 +0600

    chart two added

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,14 +1,18 @@
 
 import React from 'react';
+import MyAreaChart from '../MyAreaChart/MyAreaChart';
 import MyLineChart from '../MyLineChart/MyLineChart';
 
 const Dashboard = () => {
     return (
         <div>
-            <div className='my-8'>
-                <h1>Line Chart</h1>
-            <MyLineChart>
-            </MyLineChart>
+            <div className='my-24 ml-10'>
+                <h1 className='text-2xl text-purple-500 font-semibold mr-auto'>Investment VS Revenue</h1>
+            <MyLineChart></MyLineChart>
+            </div>
+            <div className='my-24 ml-10'>
+            <h1 className='text-2xl text-orange-500 font-semibold mr-auto'>Investment VS Revenue</h1>
+                <MyAreaChart></MyAreaChart>
             </div>
         </div>
     );
